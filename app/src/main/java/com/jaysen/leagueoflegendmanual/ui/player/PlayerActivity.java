87dfcd901840aca74c85fa91f72203823b86280c
jaysen.lin <jaysen.lin@foxmail.com>
2016-12-22 23:28:04 +0800

    add player activity

---
+++ app/src/main/java/com/jaysen/leagueoflegendmanual/ui/player/PlayerActivity.java
@@ -0,0 +1,15 @@
+package com.jaysen.leagueoflegendmanual.ui.player;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jaysen.leagueoflegendmanual.R;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
